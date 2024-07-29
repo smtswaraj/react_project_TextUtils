12230528f4eea93ad995098151adc9f86bfa280c
Swaraj Nayak <swarajnayakdipu227@gmail.com>
2024-07-29 12:11:02 +0530

    video 15 completed with adding favicon  in  Dark Mode project

--- src/App.js
+++ src/App.js
@@ -27,12 +27,19 @@ function App() {
       setMode('dark')
       document.body.style.backgroundColor = '#042743'
       showAlert("Dark mode has been enabled", "success")
+      document.title = "TextUtils - Dark Mode"
+      // setInterval(()=>{
+      //   document.title = 'TextUtils is Amazing Mode'
+      // },2000);
+      // setInterval(()=>{
+      //   document.title = 'Install TextUtils now'
+      // },1500);
     }   
     else{
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert("Light mode has been enabled", "success")
-
+      document.title = "TextUtils - Light Mode"
     }
   }
 
